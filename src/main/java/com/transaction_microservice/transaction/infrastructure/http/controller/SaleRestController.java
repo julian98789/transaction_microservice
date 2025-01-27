7298b13c679a1a303994d5e8314e93096d9f1f4e
julian98789 <julian_gomez69222@elpoli.edu.co>
2025-01-27 14:27:37 -0500

    Cree documentacion en open api para el enpoit de comprar carrito

--- src/main/java/com/transaction_microservice/transaction/infrastructure/http/controller/SaleRestController.java
+++ src/main/java/com/transaction_microservice/transaction/infrastructure/http/controller/SaleRestController.java
@@ -3,6 +3,12 @@ package com.transaction_microservice.transaction.infrastructure.http.controller;
 import com.transaction_microservice.transaction.application.dto.sale_dto.SaleReportResponse;
 import com.transaction_microservice.transaction.application.handler.sale_handler.ISaleHanddler;
 import com.transaction_microservice.transaction.domain.util.Util;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -17,6 +23,13 @@ public class SaleRestController {
 
     private final ISaleHanddler saleHandler;
 
+    @Operation(summary = "Comprar artículos del carrito", description = "Este endpoint permite a un usuario autenticado comprar los artículos que tiene en su carrito.", security = @SecurityRequirement(name = "bearerAuth"))
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Compra realizada con éxito", content = @Content(schema = @Schema(implementation = SaleReportResponse.class))),
+            @ApiResponse(responseCode = "401", description = "No autorizado", content = @Content),
+            @ApiResponse(responseCode = "403", description = "Prohibido", content = @Content),
+            @ApiResponse(responseCode = "500", description = "Error interno del servidor", content = @Content)
+    })
     @PreAuthorize(Util.ROLE_CLIENT )
     @PostMapping("/buy-cart")
     public ResponseEntity<SaleReportResponse> buyItems() {
